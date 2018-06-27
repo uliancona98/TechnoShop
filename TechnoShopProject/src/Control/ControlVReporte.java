14f55f6b628b93df5f44e98a32ec951f2a6dfa77
Juancrack97 <juanito.1397@gmail.com>
2018-06-27 12:44:02 -0500

    Add files via upload

---
+++ TechnoShopProject/src/Control/ControlVReporte.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Modelo.*;
+import View.*;
+/**
+ *
+ * @author Juan D.M
+ */
+public class ControlVReporte implements ActionListener {
+    private VReporte reporte; 
+    
+    public ControlVReporte() {
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent evento) {
+        if(reporte.getBImprimir() == evento.getSource()){
+             
+        }
+        if(reporte.getBMostrar() == evento.getSource()){
+             
+        } 
+        if(reporte.getBRegresar() == evento.getSource()){
+             
+        } 
+    }
+}
