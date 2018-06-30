1628446701ae6dab0cce7e1419bc56399d005ba7
shaidbojorquez <shaid.bojorquez@gmail.com>
2018-06-30 16:06:16 -0500

    Add files via upload

--- TechnoShopProject/src/Control/ControlVPedido.java
+++ TechnoShopProject/src/Control/ControlVPedido.java
@@ -13,19 +13,24 @@ import javax.swing.JOptionPane;
 public class ControlVPedido implements ActionListener {
     private VPedido pedido;
     private  Usuario usuario;
-    private ArrayList <Producto> productos;
+    private ArrayList <Producto> productos;//para que es el array? que recibe?
     private double totalCompra=0;
     private Producto producto;
+    Tarjeta tarjeta;
+    String precioTotal;//para convertir el totalCompra a string e imprimirlo en etiqueta Total
     public ControlVPedido(VPedido pedido, Usuario usuario, Producto producto){
         this.pedido=pedido;
         this.usuario=usuario;
         this.producto=producto;
+       
         this.totalCompra=producto.getPrecioVenta();
+         precioTotal=String.valueOf(totalCompra);//para poner el precio total en la ventana
+         pedido.getetiquetaTotal().setText(precioTotal);//imprimo el precio total en la vetana
         this.pedido.getbotonAceptar().addActionListener(this);
         this.pedido.getbotonAgregarTarjeta().addActionListener(this);
         this.pedido.getbotonCancelar().addActionListener(this);
         this.pedido.getbotonPagarPuntos().addActionListener(this);
-      
+        
         pedido.getetiquetaFechaPedido().setText(FechaActual());
     }
     public ControlVPedido(VPedido pedido, Usuario usuario,ArrayList <Producto> productos){
@@ -34,6 +39,8 @@ public class ControlVPedido implements ActionListener {
         for(int i=0;i<productos.size();i++){
             this.totalCompra=productos.get(i).getPrecioVenta()+totalCompra;
         }
+         precioTotal=String.valueOf(totalCompra);//para pasar el precio a string 
+         pedido.getetiquetaTotal().setText(precioTotal);//para imprimir el precio en la vetana
         this.pedido=pedido;
         this.usuario=usuario;
         this.productos=productos;
@@ -78,9 +85,26 @@ public class ControlVPedido implements ActionListener {
                puntosNuevos=usuario.getMembresia().getPuntos()-numeroPuntosUtilizar;
                usuario.getMembresia().setPuntos(puntosNuevos);
                 totalCompra=totalCompra-numeroPuntosUtilizar;
+                precioTotal=String.valueOf(totalCompra);
+                pedido.getetiquetaTotal().setText(precioTotal);
+                //una vez que reduce el precio cuando usa sus puntos, imprime el nuevo saldo en la ventana.
                 usuario.getMembresia().acumularPuntos(totalCompra);
+                
             }else{
                 JOptionPane.showMessageDialog(null, "No cuentas con suficientes puntos");
+            }
+            
+            
+            if(pedido.getbotonAceptarTarjeta()==evento.getSource()){
+                if(pedido.getTTitularTarjeta().getText() !=" "   && pedido.getTNumeroTarjeta().getText() !=" " ){
+                    tarjeta.setNoTarjeta(pedido.getTNumeroTarjeta().getText());
+                    usuario.setTarjeta(tarjeta);//le agrego una tarjeta al usuario
+                    JOptionPane.showMessageDialog(null, "Tarjeta Agregada");
+                    pedido.getVentanaTarjeta().setVisible(false);
+                }else{
+                    JOptionPane.showMessageDialog(null, "Debes llenar todos los campos");
+                }
+                
             }
         }
     }
