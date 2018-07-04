cd46bb9ac97c98049399a6ee1a9a116f3a134beb
Ulises Ancona <uliancona@hotmail.com>
2018-07-04 18:51:44 -0500

    Add files via upload

--- TechnoShopProject/src/technoshop/TechnoShopProject.java
+++ TechnoShopProject/src/technoshop/TechnoShopProject.java
@@ -2,6 +2,7 @@ package technoshop;
 import View.*;
 import Control.*;
 import Modelo.Conexion;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
 public class TechnoShopProject {
@@ -12,12 +13,90 @@ public class TechnoShopProject {
 	} catch (Exception e) {
             e.printStackTrace();
 	}
-        //insertarAdministradores();
-        //insertarCategoriasProductos();
+        insertarAdministradores();
+        insertarCategoriasProductos();
+        insertarProductos();
 	VHome home = new VHome();
         home.setLocationRelativeTo(null);
 	home.setVisible(true);      
         ControlVHome Chome = new ControlVHome(home);              
+    }
+    public static void insertarProductos(){
+        Object[]valoresProducto = new Object [8];
+        valoresProducto[0]="1230506930391";
+        valoresProducto[1]="Acer Aspire AC20-720-MB13";
+        valoresProducto[2]="9562.5";
+        valoresProducto[3]="7650";
+        valoresProducto[4]="Computadora de escritorio marca Acer";
+        valoresProducto[5]="20";
+        valoresProducto[6]="ACER";
+        valoresProducto[7]="1";
+        Conexion.insert("productos", valoresProducto);
+        if(Conexion.elementoInsertado){   
+            JOptionPane.showMessageDialog(null,"Producto insertado correctamente");                    
+        }
+        valoresProducto[0]="1230603847594";
+        valoresProducto[1]="Laptop Asus  E402NA-GA002T";
+        valoresProducto[2]="5375";
+        valoresProducto[3]="4300";
+        valoresProducto[4]="Laptop marca Asus";
+        valoresProducto[5]="20";
+        valoresProducto[6]="ASUS";
+        valoresProducto[7]="1";
+        Conexion.insert("productos", valoresProducto);
+        if(Conexion.elementoInsertado){   
+            JOptionPane.showMessageDialog(null,"Producto insertado correctamente");                    
+        }
+        valoresProducto[0]="1230985948038";
+        valoresProducto[1]="HP - Laptop 15-BW014LA";
+        valoresProducto[2]="10562.5";
+        valoresProducto[3]="8450";
+        valoresProducto[4]="Laptop marca Hp";
+        valoresProducto[5]="20";
+        valoresProducto[6]="HP";
+        valoresProducto[7]="1" ;
+        Conexion.insert("productos", valoresProducto);
+        if(Conexion.elementoInsertado){   
+            JOptionPane.showMessageDialog(null,"Producto insertado correctamente");                    
+        }       
+        
+        valoresProducto[0]="1290809409853";
+        valoresProducto[1]="Smartphone Techpad M6";
+        valoresProducto[2]="2496.25";
+        valoresProducto[3]="1997";
+        valoresProducto[4]="Smartphone marca Techpad";
+        valoresProducto[5]="20";
+        valoresProducto[6]="TECHPAD";
+        valoresProducto[7]="1";
+        Conexion.insert("productos", valoresProducto);
+        if(Conexion.elementoInsertado){   
+            JOptionPane.showMessageDialog(null,"Producto insertado correctamente");                    
+        }
+        
+        valoresProducto[0]="2039905409380";
+        valoresProducto[1]="Mouse Inalambrico M525";
+        valoresProducto[2]="937.5";
+        valoresProducto[3]="750";
+        valoresProducto[4]="Mouse inalambrico marca Logitech";
+        valoresProducto[5]="20";
+        valoresProducto[6]="LOGITECH";
+        valoresProducto[7]="2";
+        Conexion.insert("productos", valoresProducto);
+        if(Conexion.elementoInsertado){   
+            JOptionPane.showMessageDialog(null,"Producto insertado correctamente");                    
+        }
+        valoresProducto[0]="4695869499006";
+        valoresProducto[1]="Fifa 16";
+        valoresProducto[2]="375";
+        valoresProducto[3]="300";
+        valoresProducto[4]="videojuego para Xbox 360";
+        valoresProducto[5]="20";
+        valoresProducto[6]="XBOX";
+        valoresProducto[7]="3";
+        Conexion.insert("productos", valoresProducto);
+        if(Conexion.elementoInsertado){   
+            JOptionPane.showMessageDialog(null,"Producto insertado correctamente");                    
+        }
     }
     
     public static void insertarAdministradores(){
